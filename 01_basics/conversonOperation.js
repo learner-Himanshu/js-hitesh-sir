fc77ecbf1795a91323a1b3c53e8ee29efe2cb117
learner-Himanshu <144791918+learner-Himanshu@users.noreply.github.com>
2024-01-18 07:54:32 +0000

    convertion of datatypes

---
+++ 01_basics/conversonOperation.js
@@ -0,0 +1,22 @@
+//  let score ="33";
+
+ let score = "33abc"
+
+//  console.log(typeof(score));
+
+//  let typeInNumber = Number(score)
+//  console.log( typeof typeInNumber);
+//  console.log( typeof typeInNumber);
+//  console.log(typeInNumber);
+
+let isLoggedIn= 1;
+
+//  isLoggedIn = 0 conert into false
+//  isLoggedIn = 1 conert into true
+//  isLoggedIn = "" conert into false
+//  isLoggedIn = "abcd" conert into true
+
+console.log(typeof isLoggedIn)
+let booleanIsLoggedIn =Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
