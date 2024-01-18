ccea3297c700c3afdbcf1e038f9f23c7a83f232c
learner-Himanshu <144791918+learner-Himanshu@users.noreply.github.com>
2024-01-18 07:29:17 +0000

    Starting with Datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,35 @@
+"use strict" // we use "use strict" when we want to render our whole code accorind to latest version of javascript
+
+ const acccountId=1234;
+ let userName= "whatever"
+ var acccountPassword ="js12345"
+// alert(3+3)   we're using node js not browser..
+
+//console.log(3+3) // should we sue semicolon ?
+
+// console.log(3+3) console.log("learning Javascript..");//  if we want to execute our code according to js syntax which is correct then we have to use semicolon 
+console.log(3+3) 
+console.log("learning Javascript..") //correct way of using
+
+// But to increasing the code readability that way is not right
+// we should go for next line to write the next line of the code 
+//if we're using these steps then we can skip the use of semicolon 
+//js will never mind it //
+
+
+// datatype
+
+// Number => 2 to the power of 52-53 approximtely
+//bigInt => for the  long numbers
+//string => ""
+//Boolean => true,false
+//null => standAlone value defined value memory is provided to this according to data type
+// undefined => value is not assigned
+// symbol => uniqueness
+
+
+// =================>  console.log(typeOf undefined)  ==> undefined
+// =================>  console.log(typeOf null)       ==> Object
+
+// object =>
+
