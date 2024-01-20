f2164fb205b5c6a85d0d9ee1640649a0d438b7b0
learner-Himanshu <144791918+learner-Himanshu@users.noreply.github.com>
2024-01-20 11:31:12 +0000

    datatype summary

---
+++ 01_basics/DataType -Summary.js
@@ -0,0 +1,18 @@
+// we devide our data types into two parts 
+// 1. Primitive Data Type
+// 2. Non-Primitive Data Type
+
+
+// Primitive Data type has multiple type of datatypen
+
+//  String   
+//  Number
+//  Boolean
+//  null        type = Object
+//  undefined   type = undefined
+//  Symbol      type = Symbol
+//  BigInt
+
+// Non-primitive Data types  are --
+
+// Array,Object, Functions
