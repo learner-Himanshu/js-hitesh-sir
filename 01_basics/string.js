d7fcc838610f54f5b08babdf92c3cb4dcb5994b1
learner-Himanshu <144791918+learner-Himanshu@users.noreply.github.com>
2024-01-21 15:59:27 +0000

    string methods

---
+++ 01_basics/string.js
@@ -0,0 +1,24 @@
+const name = "sundar "
+const repoCount = 20;
+console.log(name+repoCount);
+
+// anoter way to declare the string in javascript
+
+const gameName = new String("Free-fire")
+console.log(gameName)
+
+
+// here are some strings methods ------
+
+// String toUpperCase()
+// String toLowerCase()
+// String concat()
+// String trim()
+// String trimStart()
+// String trimEnd()
+// String padStart()
+// String padEnd()
+// String repeat()
+// String replace()  The replace() method replaces a specified value with another value in a string: By default, the replace() method replaces only the first match:
+// String replaceAll()
+// String split()   A string can be converted to an array with the split() method:
