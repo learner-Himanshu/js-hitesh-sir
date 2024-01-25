c022037985fe7d797895d127624b1cae65ebbce8
Himanshu <144791918+learner-Himanshu@users.noreply.github.com>
2024-01-25 07:24:17 +0000

    ObjectDeStructuring

---
+++ 02_basics/ObjectDeStructuring.js
@@ -0,0 +1,18 @@
+const course ={
+    courseName:"javascript",
+    Price:1999,
+    courseInstructor:"Hitesh chaudhary"
+}
+// console.log(course);
+
+// destructuring 
+
+const {courseInstructor: instructor,Price,courseName}= course;
+console.log(instructor);
+
+// API
+
+// there is a famous api respomce random user me
+
+// and there is also a format chaner which name is json formatter where we can change our code in json format
+
